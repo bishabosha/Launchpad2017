8d5adc0ffa0ac32194e82622ca968cad2f5dab3e
Omar Reid <or267@bath.ac.uk>
2017-03-26 04:37:14 +0100

    Addresses and Events on Profile Page

--- advanced/frontend/views/profile/index.php
+++ advanced/frontend/views/profile/index.php
@@ -16,13 +16,15 @@ use common\models\User;
     <div class="page-header">
         <h3>Stats</h3>
         <?php
-            echo "<p> Balance: ".$user->balance."<p/>";
-            echo "<p> Events Hosted: ".$user->events_hosted."<p/>";
-            echo "<p> Events Attended: ".$user->events_attended."<p/>";
-            echo "<p> Rating: ". $user->rating."<p/>";
-            echo "<p> Date Joined: ". substr($user->joined, 0, 10)."<p/>";
+            echo "<p> Balance: ".$user->balance."</p>";
+            echo "<p> Events Hosted: ".$user->events_hosted."</p>";
+            echo "<p> Events Attended: ".$user->events_attended."</p>";
+            echo "<p> Rating: ". $user->rating."</p>";
+            echo "<p> Date Joined: ". substr($user->joined, 0, 10)."</p>";
         ?>
     </div>
+
+
 
     <div id="bio-section">
         <div class="page-header">
@@ -31,15 +33,25 @@ use common\models\User;
         <p> <?= $user->bio ?> </p>
     </div>
 
+    <div id="address-section">
+        <div class="page-header">
+            <h3>Addresses</h3>
+        </div>
+        <?php $addresses = $user->getAddresses()->all(); ?>
+        <?php foreach ($addresses as $address):?>
+            <p><?= $address->name." ". $address->street1. ", ". $address->city.", ".$address->postcode?></p>
+        <?php endforeach;?>
+    </div>
+
     <div class="page-header">
         <h3>Upcoming Events</h3>
     </div>
 
     <div class="list-group">
-
-<!--            <a href="#" class="list-group-item">Event 1</a>-->
-<!--            <a href="#" class="list-group-item">Event 2</a>-->
-<!--            <a href="#" class="list-group-item">Event 3</a>-->
+        <?php $events = $user->getEvents(); ?>
+        <?php foreach ($events as $event):?>
+            <a href="#" class="list-group-item"><?= $event->name ?></a>
+        <?php endforeach;?>
     </div>
 
 </body>
