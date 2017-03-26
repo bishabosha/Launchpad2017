9f038b591acfe31f24b4d8445c01c458eaa7a85b
eosbamsi <daniil.demishin@gmail.com>
2017-03-26 10:48:57 +0100

    added pics

--- advanced/frontend/views/site/index.php
+++ advanced/frontend/views/site/index.php
@@ -7,38 +7,33 @@ $this->title = 'Home';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>FanCy</h1>
+        <img src="<?= \yii::getAlias('@web/img/logo.jpg') ?>" alt="FanCy" style="width:304px;height:228px;">
+        <p>Food & Community</p>
+        <p>FanCy</p>
 
-        <p class="lead">Food and Community</p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Share Your Love of Food</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+
+                <h2>Spread Your Love of Food</h2>
+
+                <p>Since the beginning of times food brought people together. The whole process from picking the right ingredients to the lovely nourishment have its own charm. Enjoy it even more by spreading your passion with others and inspiring them to follow suit.</p>
             </div>
             <div class="col-lg-4">
                 <h2>Meet Interesting People In Your Area</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>We FanCy a closer community.Today, connecting is instant and easy, yet we are more isolated than ever. At FanCy we believe that sharing is not just caring but also a way to interact and meet new people. Build your own community today with us and stay connected to real people in your real life.</p>
 
             </div>
             <div class="col-lg-4">
-                <h2>Culture</h2>
+                <h2>Share The Culture</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Take the opportunity to find out more about other cultures and their unique gastronomy from all over the world, in your local community. Convenient, easy and fun, FanCy will offer you the choice of spending your time in a diversified environment and bring some ‘spice’ and ‘sugar’ to your life.
+                </p>
 
             </div>
         </div>
