ec6b1477ef381dd754aacfbb7077d7e4008d0977
Omar Reid <or267@bath.ac.uk>
2017-03-26 05:49:57 +0100

    t

--- advanced/frontend/views/event/view.php
+++ advanced/frontend/views/event/view.php
@@ -17,6 +17,34 @@ use common\models\Event;
     </div>
     <p> <?= $event->address->getFullFormat()?></p>
 </div>
+<style>
+    #map {
+        height: 400px;
+        width: 100%;
+    }
+</style>
+
+<script>
+    function initMap() {
+        <?php
+        $coder = new \common\models\GeoCoder();
+        $latlang = $coder->makeLatLngFromAddress($event->address);
+
+        ?>
+        var uluru = {lat: <?= $latlang->lat ?>, lng:  <?= $latlang->lng ?>};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: <?= $event->address->city?>
+        });
+        var marker = new google.maps.Marker({
+            position: <?= $event->address->getFullFormat() ?>,
+            map: map
+        });
+    }
+</script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=<?=\Yii::$app->params['googleAPIKey'] ?>&callback=initMap">
+</script>
 
 <div class = "description-div">
     <div class="page-header">
