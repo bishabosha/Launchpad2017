20d156d9c82ff1138162a6abf49df0e80dc34841
eosbamsi <daniil.demishin@gmail.com>
2017-03-26 07:53:38 +0100

    added commented out display nearby events

--- advanced/frontend/views/event/view.php
+++ advanced/frontend/views/event/view.php
@@ -2,6 +2,7 @@
 use common\models\Event;
 /**
  * @var $event common\models\Event
+ * @var $address common\models\Address
  */
 ?>
 
@@ -47,7 +48,100 @@ use common\models\Event;
     </div>
     <p> <?= $event->description?></p>
     <p><?= "Price: ". $event->price?>  </br>  <?= "Capacity: ". $event->capacity?>  </p>
+
+<!--    <div>-->
+<!--        <strong>Results</strong>-->
+<!--    </div>-->
+<!--    <div id="output"></div>-->
+<!--</div>-->
+<!--<div id="map"></div>-->
+<!--<!-- Replace the value of the key parameter with your own API key. -->-->
+<!--<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap">-->
+<!--    ['query' => [-->
+<!--    'address' => $address->fullFormat,-->
+<!--    'key' => \yii::$app->params['googleAPIKey']-->
+<!---->
+<!--</script>-->
+
+<!--    <script>-->
+<!--        function initMap() {-->
+<!--            var bounds = new google.maps.LatLngBounds;-->
+<!--            var markersArray = [];-->
+<!---->
+<!---->
+<!---->
+<!--            var destinationIcon = 'https://chart.googleapis.com/chart?' +-->
+<!--                'chst=d_map_pin_letter&chld=D|FF0000|000000';-->
+<!--            var originIcon = 'https://chart.googleapis.com/chart?' +-->
+<!--                'chst=d_map_pin_letter&chld=O|FFFF00|000000';-->
+<!--            var map = new google.maps.Map(document.getElementById('map'), {-->
+<!--                center: {-->
+<!--                    lat: 51.3761045,-->
+<!--                    lng: -2.3619776-->
+<!--                },-->
+<!--                zoom: 10-->
+<!--            });-->
+<!--            var geocoder = new google.maps.Geocoder;-->
+<!---->
+<!--            var service = new google.maps.DistanceMatrixService;-->
+<!--            service.getDistanceMatrix({-->
+<!--                origins: [origin1],-->
+<!--                destinations: [google.maps.LatLng objects, or google.maps.Place objects],-->
+<!--                travelMode: 'DRIVING',-->
+<!--                unitSystem: google.maps.UnitSystem.METRIC,-->
+<!--                avoidHighways: false,-->
+<!--                avoidTolls: false-->
+<!--            }, function(response, status) {-->
+<!--                if (status !== 'OK') {-->
+<!--                    alert('Error was: ' + status);-->
+<!--                } else {-->
+<!--                    var originList = response.originAddresses;-->
+<!--                    var destinationList = response.destinationAddresses;-->
+<!--                    var outputDiv = document.getElementById('output');-->
+<!--                    outputDiv.innerHTML = '';-->
+<!--                    deleteMarkers(markersArray);-->
+<!---->
+<!--                    var showGeocodedAddressOnMap = function(asDestination) {-->
+<!--                        var icon = asDestination ? destinationIcon : originIcon;-->
+<!--                        return function(results, status) {-->
+<!--                            if (status === 'OK') {-->
+<!--                                map.fitBounds(bounds.extend(results[0].geometry.location));-->
+<!--                                markersArray.push(new google.maps.Marker({-->
+<!--                                    map: map,-->
+<!--                                    position: results[0].geometry.location,-->
+<!--                                    icon: icon-->
+<!--                                }));-->
+<!--                            } else {-->
+<!--                                alert('Geocode was not successful due to: ' + status);-->
+<!--                            }-->
+<!--                        };-->
+<!--                    };-->
+<!---->
+<!--                    for (var i = 0; i < originList.length; i++) {-->
+<!--                        var results = response.rows[i].elements;-->
+<!--                        geocoder.geocode({-->
+<!--                                'address': originList[i]-->
+<!--                            },-->
+<!--                            showGeocodedAddressOnMap(false));-->
+<!--                        for (var j = 0; j < results.length; j++) {-->
+<!--                            if (results[j].distance.value < OUR_CUTOFF_DISTANCE_IN_METERS) {-->
+<!--                                geocoder.geocode({-->
+<!--                                        'address': destinationList[j]-->
+<!--                                    },-->
+<!--                                    showGeocodedAddressOnMap(true));-->
+<!--                                outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +-->
+<!--                                    ': ' + results[j].distance.text + ' in ' +-->
+<!--                                    results[j].duration.text + '<br>';-->
+<!--                            }-->
+<!--                        }-->
+<!--                    }-->
+<!--                }-->
+<!--            });-->
+<!--        }-->
+<!--    </script>-->
 </div>
+
+
 
 
 
