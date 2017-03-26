04b3a611530168dc6aeb6301e1ffadae898c8041
James Thompson <jamie.thompson@stellarise.com>
2017-03-26 04:56:16 +0100

    add google API key

--- advanced/common/config/params.php
+++ advanced/common/config/params.php
@@ -3,4 +3,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    'googleAPIKey' => 'AIzaSyAqt8r1cGGmkVRyeJ9DXzHMZJ3LFjZLw6A'
 ];
